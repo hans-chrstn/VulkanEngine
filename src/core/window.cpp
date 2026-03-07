ece46f1e6a8c2cae1144fc50ca0e2b9aeb23f192
hayato-oo <xuhiko13@gmail.com>
2026-03-07 11:16:20 -0500

    feature: initialize window loop

--- src/core/window.cpp
+++ src/core/window.cpp
@@ -8,5 +8,30 @@ namespace Engine::Core {
         if (!glfwInit()) {
             ENGINE_FATAL("Failed to initialize GLFW!");
         }
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_FALSE);
+
+        m_Window = glfwCreateWindow(m_Width, m_Height, m_Title.c_str(), nullptr,
+                                    nullptr);
+
+        if (m_Window == nullptr) {
+            ENGINE_FATAL("Failed to create a window");
+        }
+    }
+
+    Window::~Window() {
+        glfwDestroyWindow(m_Window);
+        glfwTerminate();
+    }
+
+    bool Window::shouldClose() const {
+        return glfwWindowShouldClose(m_Window);
+    }
+
+    bool Window::pollEvents() const {
+        glfwPollEvents();
+        return true;
     }
 } // namespace Engine::Core
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,19 @@
 #include "core/logger.hpp"
+#include "core/window.hpp"
+#include <GLFW/glfw3.h>
+#include <exception>
+
 int main() {
     Engine::Logger::Info("Vulkan Engine is initializing.");
+    try {
+        Engine::Core::Window window(1280, 720, "My Vulkan Engine");
+        while (!window.shouldClose()) {
+            window.pollEvents();
+            Engine::Logger::Info("Vulkan Engine has started!");
+        }
+    } catch (const std::exception &e) {
+        ENGINE_FATAL(e.what());
+        return 1;
+    }
     return 0;
 }
