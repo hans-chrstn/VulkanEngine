17d111061f76b1ee637e6b72290360731bd6da34
hayato-oo <xuhiko13@gmail.com>
2026-03-08 12:37:40 -0400

    feature: added logical device handling

---
+++ src/renderer/logical_device.cpp
@@ -0,0 +1,55 @@
+#include "logical_device.hpp"
+#include "core/logger.hpp"
+#include "physical_device.hpp"
+#include <cstdint>
+#include <set>
+#include <vulkan/vulkan_core.h>
+
+namespace Engine::Renderer {
+    VulkanLogicalDevice::VulkanLogicalDevice(
+        const VulkanPhysicalDevice &physicalDevice, VkSurfaceKHR surface) {
+        QueueFamilyIndices indices = physicalDevice.findQueueFamilies(
+            physicalDevice.getPhysicalDevice(), surface);
+
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies = {
+            indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        float queuePriority = 1.0f;
+        for (uint32_t queueFamily : uniqueQueueFamilies) {
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = queueFamily;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+        createInfo.queueCreateInfoCount =
+            static_cast<uint32_t>(queueCreateInfos.size());
+        createInfo.pEnabledFeatures = &deviceFeatures;
+        createInfo.enabledExtensionCount =
+            static_cast<uint32_t>(deviceExtensions.size());
+        createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+        createInfo.enabledLayerCount = 0;
+
+        if (vkCreateDevice(physicalDevice.getPhysicalDevice(), &createInfo,
+                           nullptr, &_device) != VK_SUCCESS) {
+            ENGINE_FATAL("Failed to create logical device");
+        }
+
+        vkGetDeviceQueue(_device, indices.graphicsFamily.value(), 0,
+                         &_graphicsQueue);
+        vkGetDeviceQueue(_device, indices.presentFamily.value(), 0,
+                         &_presentQueue);
+    }
+
+    VulkanLogicalDevice::~VulkanLogicalDevice() {
+        vkDestroyDevice(_device, nullptr);
+    }
+}; // namespace Engine::Renderer
---
+++ src/renderer/logical_device.hpp
@@ -0,0 +1,29 @@
+#include "physical_device.hpp"
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+
+namespace Engine::Renderer {
+    class VulkanLogicalDevice {
+      private:
+        VkDevice _device;
+        VkQueue _graphicsQueue;
+        VkQueue _presentQueue;
+
+      public:
+        VulkanLogicalDevice(const VulkanPhysicalDevice &physicalDevice,
+                            VkSurfaceKHR surface);
+        VulkanLogicalDevice(const VulkanLogicalDevice &) = delete;
+        VulkanLogicalDevice &operator=(const VulkanLogicalDevice &) = delete;
+        ~VulkanLogicalDevice();
+
+        VkDevice getDevice() const {
+            return _device;
+        }
+        VkQueue getGraphicsQueue() const {
+            return _graphicsQueue;
+        }
+        VkQueue getPresentQueue() const {
+            return _presentQueue;
+        }
+    };
+} // namespace Engine::Renderer
