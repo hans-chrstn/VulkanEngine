94a4db64b6ee2b4ee938c8fc8ce6a76f1d39cb89
hayato-oo <xuhiko13@gmail.com>
2026-03-08 13:20:49 -0400

    fix: add to CMakeLists and add validation checks

--- src/renderer/instance.cpp
+++ src/renderer/instance.cpp
@@ -17,6 +17,10 @@ namespace Engine::Renderer {
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_3;
 
+        if (enableValidationLayers && !checkValidationLayerSupport()) {
+            ENGINE_FATAL("Validation layers requested but not available");
+        }
+
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
